19a0a0a61dca353bb1c15856b7c14feeb3c61124
Joffrey <jojo.lecudenec@gmail.com>
2022-10-04 22:12:31 +0200

    Add default permission

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -4,6 +4,7 @@ import { Command } from '../../structures/Command';
 export default new Command({
     name: 'ping',
     description: 'Replies with Pong !',
+    default_member_permissions: '8',
     categories: path.dirname(__filename).split(path.sep).pop() as string,
     run: async ({ interaction }) => {
         await interaction.deferReply();
