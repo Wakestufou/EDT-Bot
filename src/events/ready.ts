0d1af5731e8be4df5f27d83f366e02159f1aadbd
Joffrey <jojo.lecudenec@gmail.com>
2022-10-18 01:44:30 +0200

    Ajout envoie automatique

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,10 +2,31 @@ import { Color } from '../utils/Colors';
 import { Event } from '../structures/Event';
 import Logger from '../utils/Logger';
 import cron from 'node-cron';
-import myCache from '../utils/Cache';
-import { AdeOption } from '../types/ade';
-import { EmbedBuilder } from 'discord.js';
+import getEdt from '../utils/edt';
+import { TextChannel } from 'discord.js';
 
 export default new Event('ready', (client) => {
     Logger.info('Bot is online ! ' + client.user?.tag, 'READY', Color.FgGreen);
+
+    cron.schedule('0 8 * * *', () => {
+        const date = new Date();
+
+        const embedTp1 = getEdt(date, '1');
+        const embedTp2 = getEdt(date, '2');
+
+        if (process.env.CHANNEL_ID !== undefined) {
+            const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+            console.log('oui\n');
+
+            if (channel instanceof TextChannel) {
+                console.log('ouiiiiiiii\n');
+                channel
+                    .send({
+                        embeds: [embedTp1, embedTp2],
+                    })
+                    .then(() => Logger.info('EDT sent to the channel'))
+                    .catch((e) => Logger.error('Error : ', e));
+            }
+        }
+    });
 });
